6109a4e56c2915a9a29c0c7975d0aaa93abaa200
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-01 22:26:23 +0200

    Update lab.py

--- lab1/lab.py
+++ lab1/lab.py
@@ -2,15 +2,5 @@ import math
 a=int(input("Ведіть a:="))
 b=int(input("Ведіть b:="))
 c=int(input("Ведіть c:="))   
-if a!=3 :
-    print ("Good")     
-else:
-    print("Введіть :=")
-
-if c!=0:
-    print ("Good") 
-else:
-    print ("некоректне значення") 
-    c=int(input("Ведіть c:="))
 y= a+b-c
 print(y)
