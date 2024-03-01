8aa2a26a5f63c398a758ecafd6fe2a391baed5bb
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-01 22:10:34 +0200

    Update test_lab.py

--- lab1/test_lab.py
+++ lab1/test_lab.py
@@ -1,54 +1,12 @@
-import math
-from lab import pytest
+from lab import  
 
-# Function to calculate y
-def calculate_y(a, b, c):
-    if a != 3:
-        return math.sin(2 * a) / (a - 3) + math.atan(b) / c
-    else:
-        return None
+def inc(x):
+    return x + 1
 
-# Test case for valid input
-def test_valid_input():
-    a = 4
-    b = 2
-    c = 1
-    expected_result = math.sin(2 * a) / (a - 3) + math.atan(b) / c
-    assert calculate_y(a, b, c) == expected_result
+def test_answer():
+    assert inc(3) == 5
 
-# Test case for invalid input where a is 3
-def test_invalid_a():
-    a = 3
-    b = 2
-    c = 1
-    assert calculate_y(a, b, c) == None
+   
 
-# Test case for invalid input where c is 0
-def test_invalid_c():
-    a = 4
-    b = 2
-    c = 0
-    assert calculate_y(a, b, c) == None
 
-# Test case for input where c is initially 0 but corrected later
-def test_corrected_c():
-    a = 4
-    b = 2
-    c = 0
-    corrected_c = 1
-    expected_result = math.sin(2 * a) / (a - 3) + math.atan(b) / corrected_c
-    assert calculate_y(a, b, c) == expected_result
-
-# Test case for input where a is initially 3 but corrected later
-def test_corrected_a():
-    a = 3
-    b = 2
-    c = 1
-    corrected_a = 4
-    expected_result = math.sin(2 * corrected_a) / (corrected_a - 3) + math.atan(b) / c
-    assert calculate_y(corrected_a, b, c) == expected_result
-
-def test_edge_cases():
-    # You can add more edge cases testing here if needed
-    pass
 
