5d1ce88d24aebe5cc622cce528dc07a5934f3972
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-01 22:41:25 +0200

    Update test_lab.py

--- lab1/test_lab.py
+++ lab1/test_lab.py
@@ -1,10 +1,10 @@
-from lab import  math
+from lab import  math, add
 import pytest 
 
 
-def test_y():
+def test_add():
     a = 5
     b = 3
     c = 2
-    assert y == a + b - c
+    assert add == a + b - c
 
