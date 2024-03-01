b8bff11dae09fdc7804a7bb728ab8597752326ac
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-01 22:02:42 +0200

    code

--- lab1/lab.py
+++ lab1/lab.py
@@ -1,3 +1,16 @@
-n=5
-string="Hello World "
-print(string * n)
+import math
+a=int(input("Ведіть a:="))
+b=int(input("Ведіть b:="))
+c=int(input("Ведіть c:="))   
+if a!=3 :
+    print ("Good")     
+else:
+    print("Введіть :=")
+
+if c!=0:
+    print ("Good") 
+else:
+    print ("некоректне значення") 
+    c=int(input("Ведіть c:="))
+y=(math.sin(2*a)/(a-3))+(math.atan(b)/c)
+print(y)
