56f720e603dc75df8032515619a7381dad2e7630
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-01 21:39:04 +0200

    Update test_lab.py

--- lab1/test_lab.py
+++ lab1/test_lab.py
@@ -1,11 +1,16 @@
-import pytest
-from lab.py
-def multiply_string(s, n):
-    return s * n
+import unittest
+from lab
 
-class TestMultiplyString(pytest.TestCase):
+def multiply_string(string, n):
+    return string * n
+
+class TestStringMultiplier(unittest.TestCase):
+    
     def test_multiply_string(self):
         self.assertEqual(multiply_string("Hello World ", 5), "Hello World Hello World Hello World Hello World Hello World ")
+        self.assertEqual(multiply_string("Test ", 3), "Test Test Test ")
+        self.assertEqual(multiply_string("123", 0), "")
+        self.assertEqual(multiply_string("", 10), "")
 
-if __name__ == "__main__":
-    pytest.main()
+if __name__ == '__main__':
+    unittest.main()
