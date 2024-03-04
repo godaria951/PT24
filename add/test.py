b37a3d803a8b9b75d4fe6b3a5c39cb4805994b76
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-03-04 16:41:32 +0200

    Update test.py

--- add/test.py
+++ add/test.py
@@ -1,1 +1,13 @@
-gjkl
+from my module import pytest
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Ділення на нуль неможливе"
+
+def test_divide_non_zero():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    assert divide(10, 0) == "Ділення на нуль неможливе"
