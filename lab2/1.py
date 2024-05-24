6564ebe52e222dc9606d90be2fd262fdc692f34d
godaria951 <150791419+godaria951@users.noreply.github.com>
2024-05-24 08:07:33 +0300

    Update 1.py

--- lab2/1.py
+++ lab2/1.py
@@ -1,1 +1,10 @@
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Ділення на нуль неможливе"
 
+numerator = 10
+denominator = 2
+result = divide(numerator, denominator)
+print(f"Результат ділення {numerator} на {denominator}: {result}")
